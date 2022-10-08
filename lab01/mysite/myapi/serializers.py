d260eb00c053fe7dc7eb85d32dc2a6f85ee17ca3
LionyxML <rahul.juliato@gmail.com>
2022-10-07 23:10:14 -0300

    feat: adds id to serialzier

--- lab01/mysite/myapi/serializers.py
+++ lab01/mysite/myapi/serializers.py
@@ -6,4 +6,4 @@ from .models import Hero
 class HeroSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Hero
-        fields = ("name", "alias")
+        fields = ("id", "name", "alias")
