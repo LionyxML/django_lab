e6ee155f28569b316bf61e8044d54c1c0618e29b
LionyxML <rahul.juliato@gmail.com>
2022-10-15 23:53:45 -0300

    feat: implements JWT auth

--- lab05/api/views.py
+++ lab05/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .serializers import PostSerializer
 from .models import Post
+from rest_framework.permissions import IsAuthenticated
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    permissions_classes = (IsAuthenticated,)
     queryset = Post.objects.all().order_by("created_at")
     serializer_class = PostSerializer
