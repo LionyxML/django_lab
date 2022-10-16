db5631f6d0372ea0a2d009a11f6114977c1fc24c
LionyxML <rahul.juliato@gmail.com>
2022-10-16 00:11:00 -0300

    feat: changes post field to text field

--- lab05/api/models.py
+++ lab05/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    content = models.CharField(max_length=3000)
+    content = models.TextField()
     author = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
