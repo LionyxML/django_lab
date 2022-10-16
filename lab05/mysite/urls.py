2da7b05460cbef4c94471721facc24b78de0df48
LionyxML <rahul.juliato@gmail.com>
2022-10-15 23:20:58 -0300

    feat: starting jwt

--- lab05/mysite/urls.py
+++ lab05/mysite/urls.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("api.urls"))]
+urlpatterns = [
+  path("admin/", admin.site.urls),
+  path("", include("api.urls")),
+]
+
