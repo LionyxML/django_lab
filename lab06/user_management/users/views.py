64d38aacd0cff976932e8afc78013d570eb00925
LionyxML <rahul.juliato@gmail.com>
2022-10-17 23:37:00 -0300

    feat: Fixes avatar, adds profile editing

--- lab06/user_management/users/views.py
+++ lab06/user_management/users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 
 
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
 
 
 def home(request):
@@ -63,4 +63,23 @@ class CustomLoginView(LoginView):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile is updated successfully")
+            return redirect(to="users-profile")
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(
+        request,
+        "users/profile.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
