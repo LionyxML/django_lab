a2a699373b7760e85bcce55069f61e6f6a0e5e58
LionyxML <rahul.juliato@gmail.com>
2022-10-17 23:44:56 -0300

    feat: fixes profiles directory / implements profile image resizing

--- lab06/user_management/users/models.py
+++ lab06/user_management/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -14,3 +15,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    # resizing images
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
