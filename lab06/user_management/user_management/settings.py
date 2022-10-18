a8b2cb39fad4f29597bef308935fec38da31df8b
LionyxML <rahul.juliato@gmail.com>
2022-10-17 22:02:40 -0300

    feat: adds social login overall settings

--- lab06/user_management/user_management/settings.py
+++ lab06/user_management/user_management/settings.py
@@ -66,6 +66,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
@@ -130,3 +132,9 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 30  # last number is days
 
 LOGIN_REDIRECT_URL = "/"  # so we wont be redirected to /profile
 LOGIN_URL = "login"  # defaults to /accounts/login/, but our login is in /login
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    "social_core.backends.google.GoogleOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
