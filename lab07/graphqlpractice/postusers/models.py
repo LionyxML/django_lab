3100679fab0559f53058d5ca282d60b890695f49
LionyxML <rahul.juliato@gmail.com>
2022-10-23 09:02:15 -0300

    feat: postuser models

--- lab07/graphqlpractice/postusers/models.py
+++ lab07/graphqlpractice/postusers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(maxlength=100)
+    body = models.TextField()
+    author = models.ForeignKey(
+        Author, relatedname="posts", on_delete=models.CASCADE
+    )
+
+    def str(self):
+        return self.title
